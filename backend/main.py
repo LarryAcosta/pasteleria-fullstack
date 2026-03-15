0fe0411437a74a17fce416d9bf81b0cc902d54ed
Larry Acosta <larryacostamendes@gmail.com>
2026-03-15 21:27:21 +0100

    Actualizar CORS para frontend en Vercel

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:8000",
         "http://localhost:8000",
-        "https://tu-frontend.vercel.app",
+        "https://pasteleria-fullstack-7ljjsuuxf-larry-acostas-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
